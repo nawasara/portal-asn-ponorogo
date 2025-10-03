1182c4c25e50a795dbc84fa0c47e335ec2a80ad9
Pringgo Juni S <odyinggo@gmail.com>
2025-10-03 07:46:36 +0700

    update middleware

--- app/Livewire/Pages/Guest.php
+++ app/Livewire/Pages/Guest.php
@@ -10,10 +10,15 @@ class Guest extends Component
 {
     public AppsService $appsService;
     public $apps = [];
+    
     public function mount()
     {
         $service = new AppsService();
         $this->apps = $service->getApps();
+
+        if (auth()->user()) {
+            $this->middleware('whatsapp.required');
+        }
     }
 
     public function render()
