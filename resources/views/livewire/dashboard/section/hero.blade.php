a8630a5231368d2e51befd581582757371cb65e5
Pringgo Juni S <odyinggo@gmail.com>
2025-10-20 10:14:28 +0700

    wip: add self service reset mfa

--- resources/views/livewire/dashboard/section/hero.blade.php
+++ resources/views/livewire/dashboard/section/hero.blade.php
@@ -10,7 +10,6 @@
                     <br>
                     <span class="text-emerald-600 dark:text-emerald-400">{{ auth()->user()->name }}</span>
                 @else
-
                     <span class="dark:text-white">Satu Portal, Banyak Layanan</span>
                     <br>
                     <span class="text-emerald-600 dark:text-emerald-400">Untuk ASN Ponorogo</span>
@@ -18,7 +17,9 @@
             </h1>
 
             <p class="text-slate-600 dark:text-slate-300 text-lg mb-16 sm:mb-8 text-pretty">
-                @auth <span>Sekarang anda dapat mengakses</span> @else <span>Akses</span> @endauth seluruh aplikasi ASN
+                @auth <span>Sekarang anda dapat mengakses</span>
+                @else
+                <span>Akses</span> @endauth seluruh aplikasi ASN
                 Ponorogo dalam satu tempat. Cepat, aman, dan
                 terintegrasi.
             </p>
@@ -27,21 +28,20 @@
                 @auth
                     <button
                         class="px-6 py-3 bg-emerald-600 flex text-center justify-center items-center gap-x-2 hover:bg-emerald-500 text-white rounded-lg text-lg font-medium transition transform hover:-translate-y-0.5 hover:shadow-lg">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-search-icon lucide-search">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="lucide lucide-search-icon lucide-search">
                             <path d="m21 21-4.34-4.34" />
                             <circle cx="11" cy="11" r="8" />
                         </svg>
                         Cari Aplikasi
                     </button>
                 @else
-
                     <a href="/login"
                         class="px-6 py-3 flex items-center justify-center gap-x-2 bg-emerald-600 hover:bg-emerald-500 text-white rounded-lg text-lg font-medium transition transform hover:-translate-y-0.5 hover:shadow-lg">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-log-in-icon lucide-log-in">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="lucide lucide-log-in-icon lucide-log-in">
                             <path d="m10 17 5-5-5-5" />
                             <path d="M15 12H3" />
                             <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
@@ -52,9 +52,9 @@
 
                 <a href="#support"
                     class="px-6 py-3 bg-slate-300 flex text-center justify-center items-center gap-x-2 hover:bg-slate-400 dark:text-slate-800 rounded-lg text-lg font-medium transition transform hover:-translate-y-0.5 hover:shadow-lg">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                        class="lucide lucide-info-icon lucide-info">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="lucide lucide-info-icon lucide-info">
                         <circle cx="12" cy="12" r="10" />
                         <path d="M12 16v-4" />
                         <path d="M12 8h.01" />
@@ -67,9 +67,9 @@
 
         {{-- <!-- Illustration --> --}}
         <div class="justify-center" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="100">
-            <img src="{{asset('img/undraw_hello_ccwj.svg')}}" alt="ASN illustration"
+            <img src="{{ asset('img/undraw_hello_ccwj.svg') }}" alt="ASN illustration"
                 class="sm:w-64 hidden sm:flex md:w-96 drop-shadow-lg select-none" loading="lazy">
-            <img src="{{asset('img/logo.png')}}" alt="ASN illustration"
+            <img src="{{ asset('img/logo.png') }}" alt="ASN illustration"
                 class="sm:hidden flex w-20 drop-shadow-lg select-none" loading="lazy">
         </div>
     </section>
@@ -79,7 +79,8 @@
         <hr class="text-gray-300 dark:text-gray-700 mb-16"> --}}
         <div
             class="flex mx-auto justify-center gap-x-16 bg-gray-200 dark:bg-slate-800 pt-2 2xl:pt-20 sm:rounded-tr-4xl sm:rounded-tl-4xl">
-            <a href="#app-lists" class="hover:bg-gray-300 dark:hover:text-gray-800 select-none no-underline flex bg-transparent rounded-lg py-4 px-8
+            <a href="#app-lists"
+                class="hover:bg-gray-300 dark:hover:text-gray-800 select-none no-underline flex bg-transparent rounded-lg py-4 px-8
                  items-center gap-x-3 duration-400 transition-all text-gray-800 dark:text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" class="lucide lucide-grid" width="24" height="24"
                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -91,7 +92,8 @@
                 </svg>
                 <div class="text-xl">Aplikasi</div>
             </a>
-            <a href="#integration" class="hover:bg-gray-300 dark:hover:text-gray-800 select-none no-underline flex bg-transparent rounded-lg py-4 px-8
+            <a href="#integration"
+                class="hover:bg-gray-300 dark:hover:text-gray-800 select-none no-underline flex bg-transparent rounded-lg py-4 px-8
                  items-center gap-x-3 duration-400 transition-all text-gray-800 dark:text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
--- resources/views/livewire/dashboard/section/support.blade.php
+++ resources/views/livewire/dashboard/section/support.blade.php
@@ -15,7 +15,7 @@
             </h2>
         </div>
 
-        <div class="grid sm:grid-cols-2 lg:grid-cols-2 items-center gap-6">
+        <div class="grid sm:grid-cols-3 lg:grid-cols-3 items-center gap-6">
 
             {{-- Form Bantuan Card --}}
             <a class="group flex bg-white dark:bg-gray-900 rounded-2xl border border-gray-200 dark:border-gray-700 p-6 hover:shadow-lg transition justify-between min-h-40"
@@ -38,7 +38,7 @@
                             Formulir Aduan Single Sign On (SSO)
                         </h3>
                         <p class="text-gray-600 dark:text-neutral-400">
-                            Isi formulir aduan dan tim kami akan membantu kendala SSO anda.
+                            Isi formulir aduan dan tim akan menghubungi Anda
                         </p>
                     </div>
 
@@ -46,8 +46,9 @@
                         class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold text-gray-800 dark:text-neutral-200">
                         Isi Formulir
                         <svg class="shrink-0 size-4 transition ease-in-out group-hover:translate-x-1 group-focus:translate-x-1"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round">
                             <path d="m9 18 6-6-6-6" />
                         </svg>
                     </p>
@@ -84,14 +85,51 @@
                         class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold text-gray-800 dark:text-neutral-200">
                         Dapatkan panduan
                         <svg class="shrink-0 size-4 transition ease-in-out group-hover:translate-x-1 group-focus:translate-x-1"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round">
                             <path d="m9 18 6-6-6-6" />
                         </svg>
                     </p>
                 </div>
             </a>
             <!-- End Card -->
+
+            {{-- Reset MFA Card --}}
+            <a class="group flex bg-white dark:bg-gray-900 rounded-2xl border border-gray-200 dark:border-gray-700 p-6 hover:shadow-lg transition justify-between min-h-40"
+                href="" target="_blank" data-aos="zoom-in" data-aos-duration="1000" date-aos-delay="3000">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-settings-icon lucide-settings shrink-0 size-8 text-gray-800 dark:text-white mt-0.5 me-6">
+                    <path
+                        d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" />
+                    <circle cx="12" cy="12" r="3" />
+                </svg>
+
+                <div>
+                    <div>
+                        <h3 class="block font-bold text-gray-800 dark:text-white">
+                            Reset MFA Single Sign On (SSO)
+                        </h3>
+                        <p class="text-gray-600 dark:text-neutral-400">
+                            Layanan untuk mereset Multi-Factor Authentication (MFA) SSO Anda secara mandiri
+                        </p>
+                    </div>
+
+                    <p
+                        class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold text-gray-800 dark:text-neutral-200">
+                        Reset MFA Sekarang
+                        <svg class="shrink-0 size-4 transition ease-in-out group-hover:translate-x-1 group-focus:translate-x-1"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round">
+                            <path d="m9 18 6-6-6-6" />
+                        </svg>
+                    </p>
+                </div>
+            </a>
+            {{-- End Card --}}
+
         </div>
     </section>
     <!-- End Icon Blocks -->
